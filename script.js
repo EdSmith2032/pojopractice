d3efb9ea525cd45614e2e67331cba3b7c4e46a9c
Edward Smith <213342565+EdSmith2032@users.noreply.github.com>
2025-07-30 19:15:09 -0500

    Step 2 - Exercises completed

--- script.js
+++ script.js
@@ -11,3 +11,61 @@ console.log(pet.age);
 pet.age = 6;
 console.log(pet.age);
 
+
+
+//Exercise 1
+
+const book = {
+    "title": "JavaScript for Beginners", //key = title, value =  "JavaScript for Beginners"
+    "author": "Jane Doe", // key = author , value =  Jane Doe
+    "pages": 250, // key = pages, values = 250
+    "isPublished": true // key = isPublished, value = true
+};
+
+// TODO: List all the keys as a comment
+// TODO: List all the values as a comment
+
+//Exercise 2
+
+const pet = {
+    "name": "Buddy", // value Buddy
+    "species": "Dog", // value Dog
+    "age": 5 // value 5
+};
+
+console.log(pet);
+
+// TODO: What are the values for name, species, and age? Write as comments.
+
+
+//Exercise 3
+const sprite = {
+    "name": "Hero",
+    "x": 100,
+    "y": 200
+};
+// this object repersent a sprite, assuming the main character of a game, and its starting location
+console.log(sprite);
+// TODO: What does this object represent? Write your explanation as a comment.
+
+
+//Exercise 4
+
+const car = {
+    "make": "Toyota",
+    "model": "Corolla",
+    "year": 2020, // missing comma after 2020
+    "color": "Blue"
+};
+
+// TODO: Fix the syntax error.
+// TODO: Explain what was wrong in a comment.
+console.log(car);
+
+
+
+
+
+
+
+
